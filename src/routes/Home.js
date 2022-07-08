069038a72251be13d2ba964b4da601ff8d9c89fc
Kim-menu <hyunwjin@gmail.com>
2022-07-09 00:31:27 +0900

    Using MUI

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,162 +1,254 @@
 import React, {useEffect} from "react";
-import NumberFormat from "react-number-format";
 import {Link} from "react-router-dom";
+import Box from '@mui/material/Box';
+import {InputAdornment, TextField} from "@mui/material";
+import NumberFormat from "react-number-format";
+
+
+const NumberFormatCustom = React.forwardRef(function NumberFormatCustom(props, ref) {
+    const { onChange, ...other } = props;
+
+    return (
+        <NumberFormat
+            {...other}
+            getInputRef={ref}
+            onValueChange={(values) => {
+                onChange({
+                    target: {
+                        name: props.name,
+                        value: values.value,
+                    },
+                });
+            }}
+            thousandSeparator
+            isNumericString
+        />
+    );
+});
 
 export default function Home() {
-    const [price, setPrice] = React.useState(379600000);
-    const [option, setOption] = React.useState(25476000);
-    const [payment, setPayment] = React.useState();
-    const [transferRate, setTrasferRate] = React.useState(77);
-    const [commissionRate, setCommissionRate] = React.useState(0.4);
-    const [acquisitionRate, setAcquisitionRate] = React.useState(1);
+    const price = 379600000;
+    const option = 25476000;
+    const transferRate = 77;
+    const commissionRate = 0.4;
+    const acquisitionRate = 1;
+    const [payment, setPayment] = React.useState(0);
 
-    const [premium, setPremium] = React.useState();
+    const [premium, setPremium] = React.useState('');
 
-    const [firstTransfer, setFirstTransfer] = React.useState();
-    const [secondTransfer, setSecondTransfer] = React.useState();
-    const [commission, setCommission] = React.useState();
-    const [acquisition, setAcquisition] = React.useState();
-    const [realPremium, setRealPremium] = React.useState();
-    const [budget, setBudget] = React.useState();
-    const [house, setHouse] = React.useState();
+    const [firstTransfer, setFirstTransfer] = React.useState(0);
+    const [secondTransfer, setSecondTransfer] = React.useState(0);
+    const [commission, setCommission] = React.useState(0);
+    const [acquisition, setAcquisition] = React.useState(0);
+    const [realPremium, setRealPremium] = React.useState(0);
+    const [budget, setBudget] = React.useState(0);
+    const [house, setHouse] = React.useState(0);
 
     useEffect(() => {
         setFirstTransfer(Math.round(Math.max(premium-2500000, 0)*transferRate/100));
         setCommission(Math.round((price+option)*commissionRate/100));
         setAcquisition(Math.round((price+option)*acquisitionRate/100));
         setPayment((price+option)*0.1);
-    }, [premium])
+    }, [premium, transferRate, commissionRate, acquisitionRate, price, option])
     useEffect(() => {
         setSecondTransfer(Math.round(firstTransfer*transferRate/100));
-    }, [firstTransfer])
+    }, [firstTransfer, transferRate])
     useEffect(() => {
         setRealPremium(Number(firstTransfer)+Number(secondTransfer)
             +Number(commission)+Number(acquisition)+Number(premium));
-    }, [secondTransfer])
+    }, [firstTransfer, secondTransfer, commission, acquisition, premium])
     useEffect(() => {
         setBudget(Number(realPremium)+Number(payment));
         setHouse((price+option+realPremium));
-    }, [realPremium])
+    }, [realPremium, payment, price, option])
     const changePremium = (event) => {
-        setPremium(event.target.value.replace(/,/g, ''));
+        setPremium(event.target.value);
     }
     return (
         <div>
-            <h3>사전 정의된 값은 이렇답니다!</h3>
             <Link to="dsr">Link</Link>
-            <div>
-                <label>분양가 </label>
-                <NumberFormat
-                    value={price}
-                    id="price"
-                    thousandSeparator={true}
-                    suffix="₩"
+            <h3>사전 정의된 값</h3>
+            <Box
+                sx={{
+                    padding: 2
+                }}
+            >
+                <TextField
+                    label="분양가"
+                    variant="outlined"
+                    defaultValue={price}
                     disabled
-                /><br/>
-                <label>옵션가 </label>
-                <NumberFormat
-                    value={option}
-                    id="option"
-                    thousandSeparator={true}
-                    suffix="₩"
+                    name="numberformat"
+                    InputProps={{
+                        inputComponent: NumberFormatCustom,
+                        endAdornment: <InputAdornment position="end">￦</InputAdornment>
+                    }}
+                />
+                <TextField
+                    label="옵션가"
+                    variant="outlined"
+                    defaultValue={option}
                     disabled
-                /><br/>
-                <label>계약금 </label>
-                <NumberFormat
+                    name="numberformat"
+                    InputProps={{
+                        inputComponent: NumberFormatCustom,
+                        endAdornment: <InputAdornment position="end">￦</InputAdornment>
+                    }}
+                />
+                <TextField
+                    label="계약금"
+                    variant="outlined"
                     value={payment}
-                    thousandSeparator={true}
-                    suffix="₩"
                     disabled
-                /><br/>
-                <label>양도세율 </label>
-                <NumberFormat
-                    value={transferRate}
-                    id="transferRate"
-                    suffix="%"
-                    disabled
-                /><br/>
-                <label>중개수수료율 </label>
-                <NumberFormat
-                    value={commissionRate}
-                    id="commissionRate"
-                    suffix="%"
-                    disabled
-                /><br/>
-                <label>취득세율 </label>
-                <NumberFormat
-                    value={acquisitionRate}
-                    id="acquisitionRate"
-                    suffix="%"
-                    disabled
+                    name="numberformat"
+                    InputProps={{
+                        inputComponent: NumberFormatCustom,
+                        endAdornment: <InputAdornment position="end">￦</InputAdornment>
+                    }}
                 />
-            </div>
-            <h3>원하는 정도의 손피를 입력하세요!</h3>
-            <div>
-                <label>프리미엄(손피) </label>
-                <NumberFormat
+            </Box>
+            <Box
+                sx={{
+                    padding: 2
+                }}
+            >
+                <TextField
+                    label="양도세율"
+                    variant="outlined"
+                    defaultValue={transferRate}
+                    disabled
+                    InputProps={{
+                        endAdornment: <InputAdornment position="end">%</InputAdornment>
+                    }}
+                />
+                <TextField
+                    label="중개수수료율"
+                    variant="outlined"
+                    defaultValue={commissionRate}
+                    disabled
+                    InputProps={{
+                        endAdornment: <InputAdornment position="end">%</InputAdornment>
+                    }}
+                />
+                <TextField
+                    label="취득세율"
+                    variant="outlined"
+                    defaultValue={acquisitionRate}
+                    disabled
+                    InputProps={{
+                        endAdornment: <InputAdornment position="end">%</InputAdornment>
+                    }}
+                />
+            </Box>
+            <h3>손피 입력</h3>
+            <Box
+                sx={{
+                    padding: 2
+                }}
+            >
+                <TextField
+                    label="프리미엄(손피)"
+                    variant="outlined"
                     value={premium}
-                    id="premium"
-                    placeholder="프리미엄(손피)"
-                    thousandSeparator={true}
                     onChange={changePremium}
+                    name="numberformat"
+                    InputProps={{
+                        inputComponent: NumberFormatCustom,
+                        endAdornment: <InputAdornment position="end">￦</InputAdornment>
+                    }}
                 />
-            </div>
-            <h3>계산 값들은 아래와 같아요!</h3>
-            <div>
-                <label>1차 양도소득세 </label>
-                <NumberFormat
+            </Box>
+            <h3>계산 값</h3>
+            <Box
+                sx={{
+                    padding: 2
+                }}
+            >
+                <TextField
+                    label="1차 양도소득세"
+                    variant="outlined"
                     value={firstTransfer}
-                    thousandSeparator={true}
-                    suffix="₩"
                     disabled
-                /><br/>
-                <label>2차 양도소득세 </label>
-                <NumberFormat
-                    value={secondTransfer}
-                    thousandSeparator={true}
-                    suffix="₩"
-                    disabled
-                /><br/>
-                <label>중개료 </label>
-                <NumberFormat
-                    value={commission}
-                    thousandSeparator={true}
-                    suffix="₩"
-                    disabled
-                /><br/>
-                <label>취득세 </label>
-                <NumberFormat
-                    value={acquisition}
-                    thousandSeparator={true}
-                    suffix="₩"
-                    disabled
-                /><br/>
-            </div>
-            <h3>진짜 중요한 값은 아래의 값이죠!</h3>
-            <div>
-                <label>실질 프리미엄 </label>
-                <NumberFormat
-                    className="important"
-                    value={realPremium}
-                    thousandSeparator={true}
-                    suffix="₩"
-                    disabled
-                /><br/>
-                <label>총 필요 금액 </label>
-                <NumberFormat
-                    value={budget}
-                    thousandSeparator={true}
-                    suffix="₩"
-                    disabled
-                /><br/>
-                <label>주택 매수가격 </label>
-                <NumberFormat
-                    value={house}
-                    thousandSeparator={true}
-                    suffix="₩"
-                    disabled
+                    name="numberformat"
+                    InputProps={{
+                        inputComponent: NumberFormatCustom,
+                        endAdornment: <InputAdornment position="end">￦</InputAdornment>
+                    }}
                 />
-            </div>
+                <TextField
+                    label="2차 양도소득세"
+                    variant="outlined"
+                    value={secondTransfer}
+                    disabled
+                    name="numberformat"
+                    InputProps={{
+                        inputComponent: NumberFormatCustom,
+                        endAdornment: <InputAdornment position="end">￦</InputAdornment>
+                    }}
+                />
+                <TextField
+                    label="중개료"
+                    variant="outlined"
+                    value={commission}
+                    disabled
+                    name="numberformat"
+                    InputProps={{
+                        inputComponent: NumberFormatCustom,
+                        endAdornment: <InputAdornment position="end">￦</InputAdornment>
+                    }}
+                />
+                <TextField
+                    label="취득세"
+                    variant="outlined"
+                    value={acquisition}
+                    disabled
+                    name="numberformat"
+                    InputProps={{
+                        inputComponent: NumberFormatCustom,
+                        endAdornment: <InputAdornment position="end">￦</InputAdornment>
+                    }}
+                />
+            </Box>
+            <h3>실질 가격</h3>
+            <Box
+                sx={{
+                    padding: 2
+                }}
+            >
+                <TextField
+                    label="실질 프리미엄"
+                    variant="outlined"
+                    value={realPremium}
+                    disabled
+                    name="numberformat"
+                    InputProps={{
+                        inputComponent: NumberFormatCustom,
+                        endAdornment: <InputAdornment position="end">￦</InputAdornment>
+                    }}
+                />
+                <TextField
+                    label="총 필요금액"
+                    variant="outlined"
+                    value={budget}
+                    disabled
+                    name="numberformat"
+                    InputProps={{
+                        inputComponent: NumberFormatCustom,
+                        endAdornment: <InputAdornment position="end">￦</InputAdornment>
+                    }}
+                />
+                <TextField
+                    label="아파트를 얼마에 사는셈?"
+                    variant="outlined"
+                    value={house}
+                    disabled
+                    name="numberformat"
+                    InputProps={{
+                        inputComponent: NumberFormatCustom,
+                        endAdornment: <InputAdornment position="end">￦</InputAdornment>
+                    }}
+                />
+            </Box>
         </div>
     );
 }
